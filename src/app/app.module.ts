5dbe544db701da668c8f9f706c1663df64b3614c
Shivang Joshi <shivang.joshi@metacube.com>
2023-09-05 17:50:04 +0530

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SolutionComponent } from './solution/solution.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SolutionComponent
   ],
   imports: [
     BrowserModule,
